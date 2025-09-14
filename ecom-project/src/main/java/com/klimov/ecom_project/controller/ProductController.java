43ba02b900b5c0afce12cd543bd2e3366fa5c055
klim-off <investklimov@gmail.com>
2025-09-14 14:23:32 +0300

    ResponseEntity commit

--- ecom-project/src/main/java/com/klimov/ecom_project/controller/ProductController.java
+++ ecom-project/src/main/java/com/klimov/ecom_project/controller/ProductController.java
@@ -3,10 +3,9 @@ package com.klimov.ecom_project.controller;
 import com.klimov.ecom_project.model.Product;
 import com.klimov.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,12 +17,20 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
-    @RequestMapping("/")
-    public String greet(){
-        return "Hello";
+     @GetMapping("/products")
+    //public List<Product> getAllProducts(){
+     // return service.getAllProducts();
+    public ResponseEntity<List<Product>> getAllProducts(){
+       return new ResponseEntity<>(service.getAllProducts(), HttpStatus.OK);
     }
-    @GetMapping("/products")
-    public List<Product> getAllProducts(){
-        return service.getAllProducts();
+    @GetMapping("/product/{id}")
+    //public  Product getProduct (@PathVariable int id) {
+    //     return service.getProductById(id);
+    public ResponseEntity<Product> getProduct (@PathVariable int id) {
+        Product product = service.getProductById(id);
+        if (product != null)
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- ecom-project/src/main/java/com/klimov/ecom_project/model/Product.java
+++ ecom-project/src/main/java/com/klimov/ecom_project/model/Product.java
@@ -1,5 +1,6 @@
 package com.klimov.ecom_project.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +24,7 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
     private boolean available;
     private int quantity;
