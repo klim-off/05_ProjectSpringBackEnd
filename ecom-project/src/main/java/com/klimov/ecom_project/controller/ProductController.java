2030108d681ba6f705ecd8c93f9b2cf318314b3c
klim-off <investklimov@gmail.com>
2025-09-16 06:52:07 +0300

    image add commit

--- ecom-project/src/main/java/com/klimov/ecom_project/controller/ProductController.java
+++ ecom-project/src/main/java/com/klimov/ecom_project/controller/ProductController.java
@@ -4,8 +4,10 @@ import com.klimov.ecom_project.model.Product;
 import com.klimov.ecom_project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -33,4 +35,23 @@ public class ProductController {
         else
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct (@RequestPart Product product,
+                                         @RequestPart MultipartFile imageFile){
+        try {
+            Product newProduct = service.addProduct(product, imageFile);
+            return  new ResponseEntity<>(newProduct,HttpStatus.CREATED);
+        } catch (Exception e) {
+          //  throw new RuntimeException(e);
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+     }
+     @GetMapping("/product/{productId}/image")
+     public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+            Product product = service.getProductById(productId);
+            byte [] imageFile = product.getImageData();
+            return ResponseEntity.ok().
+                    contentType(MediaType.valueOf(product.getImageType())).
+                    body(imageFile);
+     }
 }
--- ecom-project/src/main/java/com/klimov/ecom_project/model/Product.java
+++ ecom-project/src/main/java/com/klimov/ecom_project/model/Product.java
@@ -1,10 +1,7 @@
 package com.klimov.ecom_project.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -24,9 +21,14 @@ public class Product {
     private String brand;
     private BigDecimal price;
     private String category;
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+   // @JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
-    private boolean available;
-    private int quantity;
+    private boolean productAvailable;
+    private int stockQuantity;
+
+    private  String imageName;
+    private  String imageType;
+    @Lob
+    private  byte[] imageData;
 
 }
